3b0817a01413e0abf7b530748c348cb8f895016e
William Wong <william.wai.lim.wong@gmail.com>
2023-10-03 19:29:40 -0400

    adds logout in app.py

--- server/app.py
+++ server/app.py
@@ -80,11 +80,21 @@ class CheckSession(Resource):
         return {}, 204
 
 
+class Logout(Resource):
+    def delete(self):
+         if session['user_id']:
+            session['user_id']=None
+            return {"message":"Successfully logged out."}, 204
+        else:
+            return {"message":"User not logged in."}, 401
+
+
 
 api.add_resource(Homepage, '/', endpoint='/')
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(Login, '/login', endpoint='login')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
