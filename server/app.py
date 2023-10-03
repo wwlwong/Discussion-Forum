97a491a2eca26aa925bc5ea2cb723d1f488a0859
William Wong <william.wai.lim.wong@gmail.com>
2023-10-03 19:14:54 -0400

    adds homepage in app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,21 @@
 from flask import request, session, make_response
 from flask_restful import Resource
+from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api # This line will run the config.py file and initialize our app
-from models import User, Post, Role, Reply
+from models import User, Post, Role, Reply, Tag
 
 
 # All routes here!
 
+class Homepage(Resource):
+    def get(self):
+        return 'Welcome to Hot Deals: A discussion forum for all deals'
 
 
 
 
-
+api.add_resource(Homepage, '/', endpoint='/')
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
