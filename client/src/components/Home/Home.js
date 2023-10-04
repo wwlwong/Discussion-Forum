610c5271b84c8c6488b780cec505aa2359f3c22a
William Wong <william.wai.lim.wong@gmail.com>
2023-10-03 23:29:28 -0400

    adds Home.js

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+  return( 
+  <div className="home">
+    <h1 className="home-header">Hot Deals</h1>
+    <p className="home-body">Find all best deals online</p>
+    <img className="home-image" src="https://cdn-icons-png.flaticon.com/512/740/740842.png"></img>
+   
+  </div>
+  );
+}
+
+export default Home;
