b2c255c30b24c6499912643d6692c845f19b08ad
William Wong <william.wai.lim.wong@gmail.com>
2023-10-12 11:00:44 -0400

    updates navigation

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const linkStyles = {
   display: "inline-block",
@@ -13,18 +14,55 @@ const linkStyles = {
   color: "white",
 };
 
-function NavBar() {
-  return (
-  <div className="navbar">
-    <NavLink to="/" exact style={linkStyles} activeStyle={{background: "red",}}>Home</NavLink>
-    <NavLink to='/post' style={linkStyles}
-        activeStyle={{background: "red",}}>Hot Deals</NavLink>
-    <NavLink to='/login' style={linkStyles}
-        activeStyle={{background: "red",}}>Login</NavLink>
-     <NavLink to='/signup' style={linkStyles}
-        activeStyle={{background: "red",}}>Signup</NavLink>   
-  </div>
-  );
-}
 
-export default NavBar;
+const Navigation = ({ updateUser }) => {
+  const [menu, setMenu] = useState(false);
+
+  const history = useHistory();
+
+  const handleLogout = () => {
+    console.log("handle logout");
+    history.push("/");
+  };
+
+  const toggleMenu = () => setMenu((prev) => !prev);
+
+  return (
+    <div className="navigation">
+      <h1 className="nav-title">Hot Deals</h1>
+      <section className="nav-menu">
+        {menu ? (
+          <ul>
+            <li className="close-button" onClick={() => setMenu(!menu)}>
+              X
+            </li>
+            <li>
+              <Link to="/"> Home</Link>
+            </li>
+            <li>
+              <Link to="/post">All Deals</Link>
+            </li>
+            <li>
+              <Link to="/post/new">New Post</Link>
+            </li>
+            <li>
+              <Link to="/login"> Login</Link>
+            </li>
+            <li>
+              <Link to="/signup"> Signup</Link>
+            </li>
+            <li className="logout-button" onClick={handleLogout}>
+              {" "}
+              Logout{" "}
+            </li>
+          </ul>
+        ) : (
+          <div className="hamburger-menu-wrapper" onClick={toggleMenu}>
+            <GiHamburgerMenu size={30} />
+          </div>
+        )}
+      </section>
+    </div>
+  );
+};
+export default Navigation;
